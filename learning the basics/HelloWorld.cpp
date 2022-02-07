7c3afbe801f8618758a30519b47d78d47384e571
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-07 15:06:37 +0200

    Printing '1'

--- learning the basics/HelloWorld.cpp
+++ learning the basics/HelloWorld.cpp
@@ -10,9 +10,14 @@ int main(){
     cout<<"Hello World\n";
     cout<<"I'm Ahmed"; 
     cout<<endl;
-    
+
     cout<<"Hello World"<<endl;
     cout<<"I'm Ahmed";
+    cout<<endl;
+
+
+    cout<<"Hello World "<<"I'm Ahmed"<<endl<<"I live in Cairo"<<endl; // << called Extraction Operater
+
 
     return 0;
 }
