4a843bd10d316f0e9d2e6d0b4d6ab92d81ba8ffc
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-07 15:03:21 +0200

    Printing

--- learning the basics/HelloWorld.cpp
+++ learning the basics/HelloWorld.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main(){
+    // cout<<"Hello World"<<endl;
+    cout<<"Hello World ";
+    cout<<"I'm Ahmed";
+    cout<<endl;
+
+    cout<<"Hello World\n";
+    cout<<"I'm Ahmed"; 
+    cout<<endl;
+    
     cout<<"Hello World"<<endl;
+    cout<<"I'm Ahmed";
 
     return 0;
 }
