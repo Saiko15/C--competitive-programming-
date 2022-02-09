e1becf1b029636543ab28dca16845aeef11d93d3
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-09 12:02:45 +0200

    Initiallization Several Ways

--- learning the basics/Data Types And Variables/DataTypes.cpp
+++ learning the basics/Data Types And Variables/DataTypes.cpp
@@ -16,4 +16,13 @@ int main() {
     a = 55;
     // get
     cout<<a<<endl;
+    //Reassign
+    a=60;
+    cout<<"2n + 1 = "<<a * 2 + 1<<endl;
+
+    int age1; //unitialliezed: Garbage. DON'T.
+    int age2 = 22; // C-style, popular
+    int age3 (30); // constructor intiallization
+    int age4 {40}; // Modern intiallization: The Preferred 
+    return 0;
 }
