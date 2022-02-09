adc212956817436072b043d4dfed112b5c0973a5
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-09 11:50:54 +0200

    Data Types And Variables

---
+++ learning the basics/Data Types And Variables/DataTypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age = 22;
+    string name = "Ahmed";
+    bool status = true;
+    double salary = 2400.60;
+    char ch = 'S';
+
+    cout<<"My name is "<<name<<". I'm "<<age<<". My monthly salary is "<<salary<<endl; 
+}
