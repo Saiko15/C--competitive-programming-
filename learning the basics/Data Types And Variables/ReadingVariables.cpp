eaeac76d4c3f3e08071f48693d9a6bf9a3b0baf0
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-09 14:50:34 +0200

    Reading Variables '2'

---
+++ learning the basics/Data Types And Variables/ReadingVariables.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num;                        //Declare Variable in memory : num
+
+    cout<<"Enter a number\n";       //Print Enter a number
+
+    cin>>num;                       //Trying to read a number, Compiler here FREEZES, it waits you enter a numebr.
+                                        
+    cout<<"************\n";
+    cout<<2 * num + 1<<endl;
+
+    int a, b;
+
+    cout<<"Enter two numbers"<<endl;
+
+    cin>> a >> b;
+
+    cout<<"Result of Addition :"<< a + b <<"\n";
+    cout<<"Result of Multiplication :"<< a * b <<"\n";
+
+    return 0;
+}
