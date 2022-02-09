68c9622fdd104c16fb2d698f3887b3202ad0038a
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-09 20:54:25 +0200

    3rd homwork done

---
+++ learning the basics/Data Types And Variables/Variables Home work/Easy/p3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int a1, a2, a3, a4, a5, a6, a7, a8;
+    cout<<"Enter 8 numbers";
+    cin>> a1 >> a2 >> a3 >> a4 >> a5 >> a6 >> a7 >> a8;
+
+    cout<<"The Sum of Even numbers is:">> a2 + a4 + a6 + a8<<"\n";
+    cout<<"The Sum of Odd numbers is:">> a1 + a3 + a5 + a7;
+
+    return 0;
+}
