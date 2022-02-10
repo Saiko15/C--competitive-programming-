e5262cc52bd288a70ea91dc60932b526854661c6
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-10 12:03:04 +0200

    begin with operators

---
+++ learning the basics/Operators/operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 6;
+    int y = -x; // -6
+    int z = -y; // 6
+
+    bool male = true;
+    bool female = !male; // not male (false)
+}
