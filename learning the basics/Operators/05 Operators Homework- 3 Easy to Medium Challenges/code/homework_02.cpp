ac1aed5afaa96119678734d10165776ad3d9d9d7
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-10 14:17:07 +0200

    Solving 2nd Operators Homework

--- learning the basics/Operators/05 Operators Homework- 3 Easy to Medium Challenges/code/homework_02.cpp
+++ learning the basics/Operators/05 Operators Homework- 3 Easy to Medium Challenges/code/homework_02.cpp
@@ -5,10 +5,10 @@ int main() {
 
 	int a = 1, b = 1, c;
 
-	cout<<(c = a+b, a = b, b = c,
-		   c = a+b, a = b, b = c,
-		   c = a+b, a = b, b = c,
-		   c = a+b, a = b, b = c) <<endl;
+	cout<<(c = a+b, a = b, b = c, // 2 , 1 , 2
+		   c = a+b, a = b, b = c, // 3, 2, 3
+		   c = a+b, a = b, b = c, // 5, 3 , 5 
+		   c = a+b, a = b, b = c) <<endl; // 8, 5 , 8
 
 	return 0;
 }
