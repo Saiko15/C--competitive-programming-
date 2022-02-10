aab0993c95f51ea2088fb8e81d16873acb36827f
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-10 14:21:51 +0200

    Solving 3rd Operators Homework 'Great, guessed it all right'

--- learning the basics/Operators/05 Operators Homework- 3 Easy to Medium Challenges/code/homework_03.cpp
+++ learning the basics/Operators/05 Operators Homework- 3 Easy to Medium Challenges/code/homework_03.cpp
@@ -5,27 +5,27 @@ int main() {
 
 	int a = 210;
 
-	a /= 2;
-	cout<<a<<"\n";
+	a /= 2; // 105
+	cout<<a<<"\n"; //105
 
-	cout<<(a /= 3)<<"\n";
-	cout<<(a /= 5)<<"\n";
-	cout<<(a /= 7)<<"\n";
+	cout<<(a /= 3)<<"\n"; //35
+	cout<<(a /= 5)<<"\n"; //7
+	cout<<(a /= 7)<<"\n"; //1
 
-	cout<<(2+3) * (5-(-3)) / 5 / 8 <<"\n";
+	cout<<(2+3) * (5-(-3)) / 5 / 8 <<"\n"; // 1
 
 	a = 10;
-	cout<<a++ + 10<<"\n";
-	cout<<++a + 10<<"\n";
-	cout<<a-- + 10<<"\n";
-	cout<<--a + 10<<"\n";
+	cout<<a++ + 10<<"\n"; // 20 , then a =11
+	cout<<++a + 10<<"\n"; // 22 , here a = 12
+	cout<<a-- + 10<<"\n"; // 22, then a=11
+	cout<<--a + 10<<"\n"; // 20 , then a =10
 
 	int b = 20;
-	cout<<a++ + ++b<<"\n";	// Don't code this way
+	cout<<a++ + ++b<<"\n";	// Don't code this way // 31, then a = 11
 
-	cout<<a<<"\n";
-	++a+=10;	// Don't code this way
-	cout<<a<<"\n";
+	cout<<a<<"\n"; // 11
+	++a+=10;	// Don't code this way // 22
+	cout<<a<<"\n"; //22
 
 
 
