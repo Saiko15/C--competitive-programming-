7a861e02c6160d1c5b34d6a977f0e1e97425dd97
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-11 10:27:04 +0200

    adding 1st answer of 1st operator homework

---
+++ learning the basics/Operators/Homework/12 Division and Modulus Homework 1 - 3 Easy Challenges/p1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    double a, b, c, d, e;
+    cout<<"Enter 5 numbers :\n";
+    cin >> a >> b >> c >> d >> e;
+    cout<< (a+b+c+d+e)/ 5 << "\n";
+    cout<< (a+b+c) / (d+e) << "\n";
+    cout<<((a+b+c)/3)/((d+e)/2)<< "\n";
+}
