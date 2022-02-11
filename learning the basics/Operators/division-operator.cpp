32c4d664d6ed1a1c724aa32e4305d4cc986991cd
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-11 10:07:11 +0200

    adding division operator

---
+++ learning the basics/Operators/division-operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double num = 8/3.0;
+    cout<<num<<"\n";
+    cout<<(int)num; // turning double to int
+
+    cout<<"\n"<< (int) 'a' << (int)'z' << "\n"; // 97 ~ 122
+    cout<<"\n"<< (int) 'A' << (int)'Z' << "\n"; // 65 ~ 90
+    
+}
