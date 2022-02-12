e621e6a0e81f4fcc13452cc8cd2b81cb82023b05
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-12 17:11:08 +0200

    answering 1st meduim quistion in selector homework

---
+++ learning the basics/Selection/06 Selection/Homework/medium-1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    b = 0;
+
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+    cin >>a; if (a>b) b=a;
+   
+    
+    cout << b;
+    // 1 67 -9 88 -45 129 90 65 77 34
+}
