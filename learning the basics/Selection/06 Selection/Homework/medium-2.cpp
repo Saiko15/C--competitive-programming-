eaff363f496d066620a0c0b825de6a9fc30ddf7b
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-12 17:30:27 +0200

    answering 2nd meduim quistion in selector homework

---
+++ learning the basics/Selection/06 Selection/Homework/medium-2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int N, a ;
+    int b =0;
+    cin >> N;
+    // N -= 1;
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+    if (N> 0) {N -=1;    cin>>a;     if(a>b)  b=a;}
+
+    cout <<b;
+    // 5 1 3 2 4 2
+    
+}   
