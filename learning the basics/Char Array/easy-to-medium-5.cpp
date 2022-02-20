8cdaa58b03bcfe3afa148256c0ea3065f33086c0
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 09:38:30 +0200

    answering 5th easy to medium question of Char Array

--- learning the basics/Char Array/easy-to-medium-5.cpp
+++ learning the basics/Char Array/easy-to-medium-5.cpp
@@ -3,3 +3,19 @@
 // ● Input ⇒ output
 // ○ “100” ⇒ 100 300
 // ○ “0200” ⇒ 200 600
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num = 0;
+    string str;
+    cin >> str;
+    
+    for (int i = 0; i < str.size() ; i++){
+        num = num * 10 + (str[i] - '0');  // because '2' = 50 and '0' = 48 so '2' - '0' = 2 , '1' - '0' = 1;
+    }
+
+    cout << num << " " << num * 3;
+
+}
