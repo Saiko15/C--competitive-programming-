89e40adfe88c43b3b3930e1ea5192556ba8e3aa1
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 09:44:38 +0200

    answering 6th easy to medium question of Char Array

--- learning the basics/Char Array/easy-to-medium-6.cpp
+++ learning the basics/Char Array/easy-to-medium-6.cpp
@@ -3,3 +3,20 @@
 // ● Input ⇒ outputs.
 // ○ “111222aabbb” ⇒ 111 222 aa bbb
 // ○ HHHH ⇒ HHHH
+#include<iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    for (int i = 1; i < str.size() ; i++){
+        if (str[i] == str[i-1])
+            cout << str[i];
+        else
+            cout << str[i-1] << " ";
+
+        if(i == str.size() -1)
+            cout << str[i];
+    }
+}
