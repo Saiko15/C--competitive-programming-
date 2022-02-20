16ce04d65ec5d7ca6b773fa456362bb988a59a5b
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 10:12:24 +0200

    adding 2nd medium question of Char Array

---
+++ learning the basics/Char Array/medium-2.cpp
@@ -0,0 +1,10 @@
+// ● Read 2 strings, then output YES if the first smaller than or equal to second.
+// Otherwise, output NO
+// ○ Don’t use < operator to compare strings. Use loops
+// ● Input ⇒ Output
+// ○ aaa aaa ⇒ YES
+// ○ aaaaa aa ⇒ NO
+// ○ abc d ⇒ YES
+// ○ dddddddddddddd xyz ⇒ YES
+// ○ azzzzzzzz za ⇒ YES
+// ○ za azzzzzzzz ⇒ NO
