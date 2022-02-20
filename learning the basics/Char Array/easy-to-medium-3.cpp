059a0d58e531faa2ba6ae156dcc6c1179a35cde4
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 08:33:11 +0200

    adding 3rd easy to medium question of Char Array

---
+++ learning the basics/Char Array/easy-to-medium-3.cpp
@@ -0,0 +1,13 @@
+// ● Read 2 strings input and str. Print YES if the given str is a substring for the
+// string. Otherwise, print NO
+// ● Input ⇒ Outputs
+// ○ ABCDEFG ABCD ⇒ YES
+// ○ ABCDEFG ABCDEF ⇒ YES
+// ○ ABCDEFG EFG ⇒ YES
+// ○ ABCDEFG DEFG ⇒ YES
+// ○ ABCDEFG BCD ⇒ YES
+// ○ ABCDEFG DEF ⇒ YES
+// ○ ABCDEFG ACEG ⇒ NO
+// ○ ABCDEFG DG ⇒ NO
+// ○ ABCDEFG GD ⇒ NO
+// ○ ABCDEFG ABCDEFG ⇒ YES
