8edf3ff559d5da856fce112e14bb59a157eb8e95
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 09:27:55 +0200

    answering 4th easy to medium question of Char Array

--- learning the basics/Char Array/easy-to-medium-4.cpp
+++ learning the basics/Char Array/easy-to-medium-4.cpp
@@ -11,3 +11,30 @@
 // ○ ABCDEFG DG ⇒ YES
 // ○ ABCDEFG GD ⇒ NO
 // ○ ABCDEFG ABCDEFG ⇒ YES
+#include<iostream>
+using namespace std;
+
+int main() {
+    string input, str;
+    cin >> input >> str;
+
+    int j = 0;
+    bool is_subsequence = true;
+    for (int i = 0; i < input.size() ; ++i){
+        
+        if (input[i] == str[j]){
+            ++j;
+            if (j == str.size()){
+                is_subsequence = true;
+                break;
+            }
+        }
+        else
+            is_subsequence = false;
+
+    }
+    if(is_subsequence)
+        cout << "YES";
+    else
+        cout << "NO";
+}
