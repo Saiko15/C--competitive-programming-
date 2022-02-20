b0943e0635eaf87a1e1c9b48e548b6ba10057c7b
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 10:24:34 +0200

    adding 3rd medium question of Char Array

---
+++ learning the basics/Char Array/medium-3.cpp
@@ -0,0 +1,9 @@
+// ● Read a very long string of digits (at least 6 digits), and add 5555 to it.
+// ○ Don’t convert to integer, as integer has max limit for values
+// ● Input ⇒ Outputs
+// ○ “100000” ⇒ “105555”
+// ○ “10001234” ⇒ “10006789”
+// ○ “3001072” ⇒ “3006627”
+// ○ “10999999999” ⇒ “11000005554”
+// ○ “999999999999999999999999999999999” ⇒
+// “1000000000000000000000000000005554”
