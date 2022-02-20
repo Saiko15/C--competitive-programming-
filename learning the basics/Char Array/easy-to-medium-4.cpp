ff7070d90a03569a080b6a9250dbe3f135e88780
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-20 09:12:50 +0200

    adding 4th easy to medium question of Char Array

---
+++ learning the basics/Char Array/easy-to-medium-4.cpp
@@ -0,0 +1,13 @@
+// ● Read 2 strings input and str. Print YES if the given str is a subsequence for
+// the string. Otherwise, print NO
+// ● Input ⇒ Outputs
+// ○ ABCDEFG ABCD ⇒ YES
+// ○ ABCDEFG ABCDEF ⇒ YES
+// ○ ABCDEFG EFG ⇒ YES
+// ○ ABCDEFG DEFG ⇒ YES
+// ○ ABCDEFG BCD ⇒ YES
+// ○ ABCDEFG DEF ⇒ YES
+// ○ ABCDEFG ACEG ⇒ YES
+// ○ ABCDEFG DG ⇒ YES
+// ○ ABCDEFG GD ⇒ NO
+// ○ ABCDEFG ABCDEFG ⇒ YES
