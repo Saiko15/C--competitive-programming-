7a0487a6b074bafb201ec1a929c9c7c3351f46fc
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-21 14:58:22 +0200

    answering 'editing or understanding it well' 1st challenge of Functions

--- learning the basics/Functions/challenge-1.cpp
+++ learning the basics/Functions/challenge-1.cpp
@@ -10,19 +10,19 @@
 using namespace std;
 
 int max(int a, int b, int c){
-        return a + b + c;
+        return max(a, max(  b ,  c));
 }
 int max(int a, int b, int c, int d){
-        return max(a, b, c)+ d;
+        return max(a,max( b, c, d));
 }
 int max(int a, int b, int c, int d, int e){
         return max(a, max(b, c, d, e));
 }
 int max(int a, int b, int c, int d, int e, int f){
-        return max(a, b, c, d, e) + f;
+        return max(a, max( b, c, d, e ,f));
 }
 
 int main() {
     
-    cout << max (1, 2, 3, 4, 5);
+    cout << max (1, 2, 3, 4, 5, 7);
 }
