6a35c2c28d19934c8a6b18ab7caee7b38f5f0712
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-22 07:45:35 +0200

    adding 3rd practice 'triangle' of recursive function

---
+++ learning the basics/Recursive Function/12 Recursive Functions/practice-triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void print_triangle(int levels) {
+
+    if(levels == 0)
+        return;
+
+    for (int i = 0; i < levels; ++i)
+        cout << "*";
+    cout <<"\n";
+
+    print_triangle(levels - 1);
+}
+
+int main() {
+    print_triangle(20);
+}
