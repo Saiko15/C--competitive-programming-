cdc1e3f8c28123c409ad0c819e00b07678f5a5b9
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-22 10:55:07 +0200

    answering 7th easy-to-medium problem of recursive function

---
+++ learning the basics/Recursive Function/12 Recursive Functions/easy-to-medium-7.cpp
@@ -0,0 +1,28 @@
+// ● Given an array we want to accumulate it as following:
+// ○ Input 1 2 3 4 5 6
+// ○ Output array
+// ■ 1, 1+2, 1+2+3, 1+2+3+4, 1+2+3+4+5, 1+2+3+4+5+6
+// ■ 1, 3, 6, 10, 15, 21
+// ○ That is return arr[i] = sum of all numbers from 0 to i
+// ● void accumulate_arr(int arr[], int len);
+// ○ Input 1 8 2 10 3 ⇒ 1 9 11 21 24 
+#include<iostream>
+using namespace std;
+
+void accumulate_arr(int arr[], int len){
+    if (len == 1)
+        return;
+    
+    accumulate_arr(arr, len-1);
+    arr[len-1] += arr[len-2];
+    
+}   
+
+int main() {
+    int arr[] = { 1, 8, 2, 10, 3};
+    accumulate_arr(arr, 5);
+
+    for (int i = 0; i < 5; ++i) {
+        cout <<arr[i] <<" ";
+    }
+}
