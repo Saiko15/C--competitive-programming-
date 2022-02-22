592fc930f16285a96c52f80638d479c88792e060
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-22 14:11:50 +0200

    answering 6th medium-to-hard problem of recursive function

---
+++ learning the basics/Recursive Function/12 Recursive Functions/medium-to-hard-6.cpp
@@ -0,0 +1,27 @@
+// ● Without running code on the right
+// ○ Trace by hand: What does this method do?
+// ○ What happens if we swapped lines 9 & 10?
+#include<iostream>             
+using namespace std;
+
+void do_something1(int n) {	// print number digit by digit reversed
+	if (n) {
+		cout << n % 10;
+		do_something1(n / 10);
+	}
+}
+
+void do_something2(int n) {
+	if (n) {
+		do_something2(n / 10);
+		cout << n % 10;
+	}
+}
+
+int main() {
+	do_something1(123456);
+	cout << "\n";
+	do_something2(123456);
+
+	return 0;
+}
