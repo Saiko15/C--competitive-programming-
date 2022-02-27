ee3d08b611d9e4e5b1864d766f5c5610f3b13637
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-27 11:23:24 +0200

    working on 'Library' project v1.3.1

--- learning the basics/project-2/Library.cpp
+++ learning the basics/project-2/Library.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 
-int length;
+int length_of_books, length_of_users, length_of_books_borrowed_by_user ;
 
 struct book{
     int id, quantity, borrowed_books_ids;
     string name, who_borrowed;
 
-
-
     void add_book() {
         cout << "Enter book info : id & name& total quantity : ";
         cin >> id >> name >> quantity;
-        length++;
+        length_of_books++;
     }
 
-    
+    void print(){
+         cout << "id = " << id << " name = " << name<< " total quantity = " << quantity<< " total borrowed = " << borrowed_books_ids <<"\n";
+    }
 
-   
 };
 
 book books[100];
@@ -28,9 +27,9 @@ book books[100];
 //         string n;
 //         cout << "Enter book name prefix :";
 //         cin >> n;
-//         for (int i = 0; i < length; ++i){
+//         for (int i = 0; i < length_of_books; ++i){
 //             if (n =< books[i].name){
-//                 for (int j = 0; j < length; ++j)
+//                 for (int j = 0; j < length_of_books; ++j)
 //                     cout << books[j].name << "\n";
 //             }
 //             else
@@ -45,17 +44,50 @@ bool compare_id(book &a, book& b){
 
     }
 
+bool compare_name(book &a, book& b) {
+    if (a.name < b.name)
+        return true;
+    return false;
+}
+
 void print_library_by_id() {
-    sort(books, books + length , compare_id );
-    for (int i = 0; i < length; ++i){
-        cout << "id = " << books[i].id << " name = " << books[i].name<< " total quantity = " << books[i].quantity<< " total borrowed = " << books[i].borrowed_books_ids <<"\n";
+    sort(books, books + length_of_books , compare_id );
+    for (int i = 0; i < length_of_books; ++i){
+        books[i].print();
+    }
+}
+
+void print_library_by_name() {
+    sort(books, books + length_of_books, compare_name);
+    for (int i = 0; i < length_of_books; ++i){
+        books[i].print();
     }
 }
 
 struct user{
-    int id[100];
-    string name[100], borrowed[100];
+    int id;
+    string name;
+
+    void add_user() {
+        cout << "Enter user name & national id: ";
+        cin >> name >> id;
+        length_of_users++;
+    }
 };
+
+user users[100];
+
+void user_borrow_book(int &name) {
+    book borrowed[100];
+    cout << "Enter user name and book name : ";
+    int len = length_of_books_borrowed_by_user;
+    cin >> name >> users.borrowed[len];
+    len++;
+
+    if(users.borrowed[len] == )
+
+}
+
 
 
 int menu(){
@@ -84,10 +116,16 @@ void run() {
     if(choice > 10 || choice < 1)
         cout << "Invalid choice. Try again\n";
     else if (choice == 1)
-        books[length].add_book();
+        books[length_of_books].add_book();
 
     else if (choice == 4)
         print_library_by_id();
+
+    else if (choice == 5)
+        print_library_by_name();
+
+    else if (choice == 6)
+        users[length_of_users].add_user();
 
 
 
